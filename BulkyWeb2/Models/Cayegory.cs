15dc206d02347c7a2ea5e6d55879ec5c5b06f8df
Muhammad Mudasir <muhmudasir4747@gmail.com>
2025-04-14 21:33:21 +0500

    Initial commit

---
+++ BulkyWeb2/Models/Cayegory.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb2.Models
+{
+    public class Cayegory
+    {
+        //we will be creating the properties here  and now all the properties
+        //that we will create in category class will be basically the cpoloumns
+        //that we want in category class
+        [Key]
+        public int Category__Id { get; set; } //Primary key of the table
+
+        [Required(ErrorMessage ="Please enter the name for the category!")]
+        public string Name { get; set; }
+
+        
+        public int DisplayOrder { get; set; }
+    }
+}
