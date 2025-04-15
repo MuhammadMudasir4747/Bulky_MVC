5ee15dbe1ac193da62366fb189dab6899b426575
Muhammad Mudasir <muhmudasir4747@gmail.com>
2025-04-15 23:24:29 +0500

    created a table in database ssms with connection string

---
+++ BulkyWeb2/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using BulkyWeb2.Models;
+
+
+namespace BulkyWeb2.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+           
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- BulkyWeb2/Models/Category.cs
+++ BulkyWeb2/Models/Category.cs
@@ -2,13 +2,13 @@
 
 namespace BulkyWeb2.Models
 {
-    public class Cayegory
+    public class Category
     {
         //we will be creating the properties here  and now all the properties
         //that we will create in category class will be basically the cpoloumns
         //that we want in category class
         [Key]
-        public int Category__Id { get; set; } //Primary key of the table
+        public int Category21Id { get; set; } //Primary key of the table
 
         
         public string Name { get; set; }
